650a4efedc0f838ec090f67dbf189b081ec4ada5
Edyta <ulanowicz.edyta@gmail.com>
2022-06-03 21:36:10 +0200

    reformat index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const YEAR = 1991;
 
 function checkYear() {
     let msg;
-    if ((YEAR % 400) === 0 || (YEAR % 4) === 0)
-     {msg = `${YEAR} is a leap year.`}
-     else {msg = `${YEAR} isn't a leap year.`}
+    if ((YEAR % 400) === 0 || (YEAR % 4) === 0) {
+         msg = `${YEAR} is a leap year.`
+        } else {
+        msg = `${YEAR} isn't a leap year.`
+        }
      return msg;
 };
 
